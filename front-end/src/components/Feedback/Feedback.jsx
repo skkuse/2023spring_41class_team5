7a30712ed82fb75c9ba35b427119dd3489a4e767
2je0 <bulljy@naver.com>
2023-05-30 01:09:53 +0900

    ✨ feat: feedback 페이지 result 알림

--- front-end/src/components/Feedback/Feedback.jsx
+++ front-end/src/components/Feedback/Feedback.jsx
@@ -1,8 +1,18 @@
-import { Text } from "components";
-import React from "react";
+import { Button, Text } from "components";
+import React, { useState } from "react";
 import CodeEditor from "@uiw/react-textarea-code-editor";
+import Result from "./Result";
+import { useNavigate } from "react-router";
 const Feedback = ({ code, feedback }) => {
-  return (
+  const [isClicked, setIsClicked] = useState(false);
+  const navigate = useNavigate();
+  return !isClicked ? (
+    <Result
+      onClickFeedback={() => {
+        setIsClicked(true);
+      }}
+    />
+  ) : (
     <>
       <div className="bg-blue_gray_900 flex flex-col font-dnfbitbitotf items-center justify-end mx-auto w-full">
         <div className="md:h-[1460px] h-[710px] sm:h-[730px] max-w-[1160px] mt-5 mx-auto md:px-5 relative w-full">
@@ -25,7 +35,7 @@ const Feedback = ({ code, feedback }) => {
           <div className="absolute bg-gray_900 flex flex-col inset-x-[0] items-center justify-end mx-auto outline outline-[0.5px] outline-gray_700 p-[15px] rounded-[5px] top-[11%] w-[67%]">
             <div className="flex flex-col gap-[11px] items-center justify-start mt-[5px] w-[99%] md:w-full">
               <div className="flex md:flex-col gap-5  w-full">
-                <div className="basis-3/6 bg-blue_gray_900_01 flex flex-col gap-[15px] items-start justify-center outline outline-[0.5px] outline-blue_gray_900 p-[15px] rounded-[5px]">
+                <div className="basis-3/6 bg-blue_gray_900_01 flex flex-col gap-[15px] items-start outline outline-[0.5px] outline-blue_gray_900 p-[15px] rounded-[5px]">
                   <Text
                     className="font-dnfbitbitotf ml-1.5 md:ml-[0] mt-[9px] text-teal_A700_01"
                     as="h3"
@@ -65,13 +75,16 @@ const Feedback = ({ code, feedback }) => {
                   </Text>
                 </div>
               </div>
-              <Text
+              <Button
                 className="bg-blue_700 h-11 justify-center sm:px-5 px-[35px] py-2.5 rounded-[5px] text-white_A700 w-[145px]"
                 as="h3"
                 variant="h3"
+                onClick={() => {
+                  navigate("/", { replace: true });
+                }}
               >
                 확인
-              </Text>
+              </Button>
             </div>
           </div>
         </div>
