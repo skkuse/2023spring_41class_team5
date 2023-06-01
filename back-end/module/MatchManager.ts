f9f428a81e987c369ece5570e65d2b6d62fa358c
openkmj <openkmj@gmail.com>
2023-06-01 16:33:56 +0900

    fix: health check bug

--- back-end/module/MatchManager.ts
+++ back-end/module/MatchManager.ts
@@ -73,6 +73,7 @@ class MatchManagerClass {
     if (idx === -1) return
     clearTimeout(this.match[idx].timer1)
     clearTimeout(this.match[idx].timer2)
+    clearTimeout(this.match[idx].endTimer)
     this.match.splice(idx, 1)
   }
   healthCheck(mid: number, uid: number, callback: () => void) {
@@ -86,7 +87,7 @@ class MatchManagerClass {
       }, HEALTH_CHECK_TIMEOUT)
     } else if (target.uid2 === uid) {
       clearTimeout(target.timer2)
-      target.timer1 = setTimeout(() => {
+      target.timer2 = setTimeout(() => {
         callback()
         this.deleteMatch(mid)
       }, HEALTH_CHECK_TIMEOUT)
